0e4301de8748f5a65be482675add0d1629c8756f
TobiasGudbjerg <togu22ab@student.cbs.dk>
2023-12-06 10:55:07 +0100

    Update chatRoutes.js

---
+++ src/routes/chatRoutes.js
@@ -0,0 +1,12 @@
+// routes/chstRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const session = require('express-session');
+
+router.get("/chat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/pages/chat.html"));
+  });
+  
