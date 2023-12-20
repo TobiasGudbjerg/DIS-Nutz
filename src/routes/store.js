99d3d45a82fc08dec46451b53be7cbec765e2cc7
TobiasGudbjerg <togu22ab@student.cbs.dk>
2023-12-20 10:21:47 +0100

    Update store.js

--- src/routes/store.js
+++ src/routes/store.js
@@ -31,10 +31,10 @@ storeRoutes.post("/checkout", (req, res) => {
   console.log("Checkout items:", bagItems);
 
   let phonenumber = req.session.telephone 
-  let phonenumberS = phonenumber.toString();
+  let phonenumberS = "phonenumber"
   var num = "+47";
   var landCode = num.toString();
-  console.log(phonenumber)
+  console.log(phonenumberS)
   let order = req.session.bagItems
   
   let message = `Your order has been received! Your order will be ready in 10 minutes.\nOrder Details:\n${order}`;
@@ -43,7 +43,7 @@ storeRoutes.post("/checkout", (req, res) => {
     .create({
         body: message ,
         messagingServiceSid: 'MGc7c78d76b29a769c368622d0c696c50c',
-        to: landCode + "46547353"
+        to: landCode + phonenumberS
     })
     .then(message => console.log(message.sid))
     .done();
