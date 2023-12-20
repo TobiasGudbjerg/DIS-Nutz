152abc3ac898743dfab0f141912f1657215c5994
TobiasGudbjerg <togu22ab@student.cbs.dk>
2023-12-20 12:49:42 +0100

    plis

--- client/scripts/chat.js
+++ client/scripts/chat.js
@@ -2,7 +2,7 @@ const socket = io();
 
 const messages = document.getElementById("messages");
 const input = document.getElementById("input");
-const username = getCookie('user'); // Retrieve username from cookie
+const username = getCookie('user') || 'Anonymous';
 
 socket.emit("user joined", username);
 
@@ -32,7 +32,13 @@ document.getElementById("logoutButton").addEventListener("click", function() {
 
 // Helper function to get cookie by name
 function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
+
--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -6,16 +6,12 @@ const path = require('path');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
-// Use cookieParser middleware
 router.use(cookieParser());
 
 router.get("/chat", (req, res) => {
-  // Access cookies like this:
   const userCookie = req.cookies.user;
-  console.log(userCookie); // This will log the 'user' cookie value
+  console.log(userCookie);
   res.sendFile(path.join(__dirname, "../../client/pages/chat.html"));
 });
-
-// Additional chat-related routes can go here
 
 module.exports = router;
